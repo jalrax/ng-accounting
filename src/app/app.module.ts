9e190fc5aae1374fb78e5bf84ab4a66b04e84ecc
Repina Elena <elena1902881@gmail.com>
2018-02-10 14:45:06 +0300

    Добавлены шаблоны

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-
 import {AppComponent} from './app.component';
 import {AuthModule} from './auth/auth.module';
 import {AppRoutingModule} from './app-routing.module';
 import {UsersService} from './shared/services/users.service';
 import {HttpClientModule} from '@angular/common/http';
 import {AuthService} from './shared/services/auth.service';
+import {SystemModule} from './system/system.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AuthModule,
+    HttpClientModule,
     AppRoutingModule,
-    HttpClientModule
+    AuthModule,
+    SystemModule
   ],
   providers: [UsersService, AuthService],
   bootstrap: [AppComponent]
---
+++ src/app/system/records-page/records-page.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'sert-records-page',
+  templateUrl: './records-page.component.html',
+  styleUrls: ['./records-page.component.scss']
+})
+
+export class RecordsPageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
